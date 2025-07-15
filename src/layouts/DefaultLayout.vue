682c49791b678241cacc9ef6dad7fb77095751c3
Cliente <gabrielbromo@gmail.com>
2025-07-15 09:03:07 -0300

    chore: criar estrutura inicial de pastas (components, sections, pages, services, interfaces, layouts...)

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Header />
+    <main class="flex-1">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from '@/sections/Header.vue'
+import Footer from '@/sections/Footer.vue' // se for usar
+</script>
