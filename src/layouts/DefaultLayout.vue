70bf2ce389f65c74e6b1a60d523650d575df5892
Cliente <gabrielbromo@gmail.com>
2025-07-15 10:37:50 -0300

    feat: show item summary in footer with 'View cart' button

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-gray-50 text-gray-800"> 
+  <div class="min-h-screen flex flex-col bg-gray-50 text-gray-800">
     <main class="flex-1 container mx-auto p-4">
       <router-view />
     </main>
-    <Footer></Footer>
+    <Footer />
   </div>
 </template>
-<script setup lang="ts"> 
-import Footer from '@/sections/Footer.vue' 
+
+<script setup lang="ts">
+import Footer from '@/sections/Footer.vue'
 </script>
