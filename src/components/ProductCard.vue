2c85c2902594c48f6880e70c5201c7c82ee0f34e
Cliente <gabrielbromo@gmail.com>
2025-07-15 10:04:36 -0300

    11. feat: create ProductCard component and display products with 'Add' button'

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-white rounded-xl shadow p-4 flex flex-col justify-between h-full">
+    <img
+      :src="product.image_url"
+      :alt="product.name"
+      class="w-full h-40 object-contain mb-3"
+      @error="onImageError"
+    />
+
+    <div class="flex-1">
+      <h2 class="text-lg font-semibold truncate">{{ product.name }}</h2>
+      <p class="text-sm text-gray-500">{{ product.brand }} • {{ product.category }}</p>
+    </div>
+
+    <div class="mt-4 flex justify-between items-center">
+      <span class="text-green-600 font-bold text-lg">R$ {{ product.price.toFixed(2) }}</span>
+      <button
+        class="bg-blue-600 text-white text-sm px-3 py-1 rounded hover:bg-blue-700"
+        @click="$emit('add', product)"
+      >
+        Adicionar
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Product } from '@/interfaces/Product'
+
+defineProps<{
+  product: Product
+}>()
+
+function onImageError(event: Event) {
+  const target = event.target as HTMLImageElement
+  target.src = 'https://placehold.co/600x400?text=Imagem+Indisponível'
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,8 @@
 import Header from '@/sections/Header.vue'
 import { useProductController } from '@/controller/productController'
 import type { ProductFilter } from '@/interfaces/ProductFilter'
+import ProductCard from '@/components/ProductCard.vue'
+import type { Product } from '@/interfaces/Product'
 
 const { products, isLoading, error, loadProducts } = useProductController()
 
@@ -9,6 +11,12 @@ loadProducts() // carregar sem filtro ao montar
 
 function onFilter(filters: ProductFilter) {
   loadProducts(filters)
+}
+
+
+function onAddToCart(product: Product) {
+  console.log('Adicionar ao carrinho:', product)
+  // Em breve: chamar orderService
 }
 </script>
 
@@ -18,10 +26,13 @@ function onFilter(filters: ProductFilter) {
   <div class="p-4">
     <div v-if="isLoading">Carregando produtos...</div>
     <div v-else-if="error" class="text-red-500">{{ error }}</div>
-    <div v-else class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <div v-for="product in products" :key="product.id">
-        {{ product.name }}
-      </div>
+    <div v-else class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"> 
+        <ProductCard
+          v-for="product in products" 
+          :key="product.id"
+          :product="product"
+          @add="onAddToCart"
+        /> 
     </div>
   </div>
 </template>
